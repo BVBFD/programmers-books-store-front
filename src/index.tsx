1d32373b7e0ef9bfa7020df2905d17841b7345d9
BVBFD <lsevina126@gmail.com>
2024-02-21 16:12:17 +0900

    Feat(styled-component): 코드 구조 및 styled-component 도입 및 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Layout from "./components/layout/Layout";
+import GlobalStyle from "./style/global";
+import { ThemeProvider } from "styled-components";
+import { dark, light } from "./style/theme";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={light}>
+      <GlobalStyle themeName={`${light.name}`} />
+      <Layout>
+        <App />
+      </Layout>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
